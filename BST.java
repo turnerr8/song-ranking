30c2f93702be0d3709c06cd6d233cab678b0cbfd
Ryan Turner <turnerr8@wwu.edu>
2021-05-02 00:34:51 -0700

    tree works

--- BST.java
+++ BST.java
@@ -1,18 +1,25 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class BST {
+    Scanner console = new Scanner(System.in);
+
     
-    public static class Node {
+    class Node {
         String data;
         Node left;
         Node right;
         Node parent;
-    }
+    
 
-    public Node(String data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-        this.parent = null;
 
+        public Node(String data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+            this.parent = null;
+
+        }
     }
 
     public Node root;
@@ -21,11 +28,52 @@ public class BST {
         root = null;
     }
 
-    public void insert(String data) {
-        if (root == null) {
-            root = new Node(data);
-        } else {
+    public void createTree(Scanner songs) {
+        String currentSong = "";
+        if(songs.hasNextLine()) {
+            currentSong= songs.nextLine();
+        }
+        if(root==null) {
+            root = new Node(currentSong);
+        } 
+        while(songs.hasNextLine()) {
+            currentSong = songs.nextLine();
+            treeCont(currentSong, root);
             
+
         }
     }
+
+    private void treeCont(String currentSong, Node current) {
+        System.out.println("Which do you like more?");
+        System.out.println("[A] " + current.data + " or [B] " + currentSong);
+        char choice = console.next().charAt(0);
+        if('a' == Character.toLowerCase(choice)) {
+            if(current.left == null) {
+            current.left = new Node(currentSong);
+            return;
+            }
+            treeCont(currentSong, current.left);
+            return;
+        } else {
+            if(current.right == null) {
+                current.right = new Node(currentSong);
+                return;
+            }
+            treeCont(currentSong, current.right);
+            return;
+        }
+
+    }
+
+    public void printTree(Node current) {
+        if(current == null) {
+            return;
+        }
+        printTree(current.right);
+        System.out.println(current.data);
+        printTree(current.left);
+    }
+    
+
 }
--- main.java
+++ main.java
@@ -1,5 +1,16 @@
-public class main {
-    public static void main(String args[]) {
-        
-    }
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class main{
+
+public static void main(String[] args) throws FileNotFoundException {
+    Scanner console = new Scanner(System.in);
+    Scanner text = new Scanner(new File("./song_files/phoebe_bridgers.txt"));
+    BST phoebe = new BST();
+    phoebe.createTree(text);
+    phoebe.printTree(phoebe.root);
+}
+
+
 }
